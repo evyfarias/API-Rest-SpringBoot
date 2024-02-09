6cd5e59c66a0a1a9e0be5a37ce1911913ba82f99
Evelyn Farias <evelyn.fsilva@sptech.school>
2024-02-09 09:01:02 -0300

    Paginação ordenada de Medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,9 +31,22 @@ public class MedicoController {
      Para controlar a quantidade de registros e a página que será consultada, basta
      modificar a url
      http://localhost:8080/medicos?size=2&page=1 -> ?size=2&page=1
+
+     Para controlar a ordenação dos dados, utilizar o SORT
+     http://localhost:8080/medicos?sort=especialidade
+
+     Para a ordenação ser decrescente, usar o DESC
+     http://localhost:8080/medicos?sort=especialidade,desc
+
+     Utilizando todos os parâmetros
+      http://localhost:8080/medicos?sort=nome,desc&size=2&page=1
+
+      -> Quem controla isto é a Interface Pageable e o retorno Page
+      -> O uso do @PageableDefault ocorre de forma default, caso não haja os parâmetros
+      na URL
     */
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao){
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 1, sort = {"nome"}) Pageable paginacao){
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
